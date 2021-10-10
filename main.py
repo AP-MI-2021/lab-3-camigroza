a375fdff55c75504f7e8eea6fa8bb8088ea9f7dc
Camelia Groza <camelia.groza1606@yahoo.com>
2021-10-10 21:27:40 +0300

    commit

---
+++ main.py
@@ -0,0 +1,126 @@
+def print_menu():
+    print("1. Citire date")
+    print("2. Determinare cea mai lunga subsecventa cu proprietatea ca produsul numerelor este impar")
+    print("3. Determinare cea mai lunga subsecventa cu proprietatea ca concatenarea numerelor din subsecventa are cifrele in ordine crescatoare")
+    print("4. Iesire")
+
+def citire_lista():
+    l = []
+    n = int(input("Dati numarul de elemente: "))
+    for i in range(n):
+        l.append(int(input("l[" + str(i) + "]=")))
+    return l
+
+def produsul_numerelor_este_impar(l):
+    '''
+    Determina produsul numerelor dintr-o lista
+    :param l: o lista de numerele intregi
+    :return: True, daca produsul este impar, False in caz contrar
+    '''
+    if len(l)==0:
+        return False
+    produs=1
+    for x in l:
+        produs=produs*x
+    if produs%2==1:
+        return True
+    return False
+
+def test_produsul_numerelor_este_impar():
+    '''
+    Functie de test
+    '''
+    assert produsul_numerelor_este_impar([]) == False
+    assert produsul_numerelor_este_impar([1,3,5]) == True
+    assert produsul_numerelor_este_impar([2]) == False
+    assert produsul_numerelor_este_impar([11,7]) == True
+
+def get_longest_product_is_odd(l):
+    '''
+    Determinare cea mai lunga subsecventa cu proprietatea ca produsul numerelor este impar
+    :param l: o lista de numere intregi
+    :return: cea mai lunga subsecventa din l cu proprietatea ca produsul numerelor este impar
+    '''
+    subsecventa_maxima = []
+    for i in range(len(l)):
+        for j in range(i,len(l)):
+            if produsul_numerelor_este_impar(l[i:j+1]) and len(subsecventa_maxima) < len(l[i:j+1]):
+                subsecventa_maxima = l[i:j+1]
+    return subsecventa_maxima
+
+def test_get_longest_product_is_odd():
+    '''
+    Functie de test
+    '''
+    assert get_longest_product_is_odd([1,2,3,5]) == [3, 5]
+    assert get_longest_product_is_odd([1]) == [1]
+    assert get_longest_product_is_odd([4, 2, 3, 5, 7]) == [3, 5, 7]
+
+def concatenarea_numerelor_ordine_crescatoare(l):
+    '''
+    Concateneaza numerele dintr-o lista
+    :param l: o lista de numere naturale
+    :return: True, daca concatenarea are cifrele in ordine crescatoare, False in caz contrar
+    '''
+    if len(l) == 0:
+        return False
+    numar_format = l[0]
+    for i in range(1,len(l)):
+        numar_format = int(str(numar_format) + str(l[i]))
+    while numar_format > 9:
+        if numar_format%10 < numar_format//10%10:
+            return False
+        numar_format = numar_format//10
+    return True
+
+def test_concatenarea_numerelor_ordine_crescatoare():
+    '''
+    Functie de test
+    '''
+    assert concatenarea_numerelor_ordine_crescatoare([1,23,4]) == True
+    assert concatenarea_numerelor_ordine_crescatoare([]) == False
+    assert concatenarea_numerelor_ordine_crescatoare([12,345,2]) == False
+    assert concatenarea_numerelor_ordine_crescatoare([1,2,3,4,56789]) == True
+
+def get_longest_concat_digits_asc(l):
+    '''
+    Determina cea mai lunga subsecventa cu proprietatea ca concatenarea numerelor din subsecventa are cifrele in ordine crescatoare
+    :param l: o lista de numere naturale
+    :return: cea mai lunga subsecventa din l cu proprietatea ca concatenarea numerelor din subsecventa are cifrele in ordine crescatoare
+    '''
+    subsecventa_maxima = []
+    for i in range(len(l)):
+        for j in range(i,len(l)):
+            if concatenarea_numerelor_ordine_crescatoare(l[i:j+1]) and len(subsecventa_maxima) < len(l[i:j+1]):
+                subsecventa_maxima = l[i:j+1]
+    return subsecventa_maxima
+
+def test_get_longest_concat_digits_asc():
+    '''
+    Functie de test
+    '''
+    assert get_longest_concat_digits_asc([1,3,1,23,4,1]) == [1,23,4]
+    assert get_longest_concat_digits_asc([9,7,4,3]) == [9]
+    assert get_longest_concat_digits_asc([4,4,2,1,2]) == [4,4]
+
+def main():
+    test_produsul_numerelor_este_impar()
+    test_get_longest_product_is_odd()
+    test_concatenarea_numerelor_ordine_crescatoare()
+    test_get_longest_concat_digits_asc()
+    l = []
+    while True:
+        print_menu()
+        optiune = int(input("Dati optiunea: "))
+        if optiune == 1:
+            l = citire_lista()
+        elif optiune == 2:
+            print(get_longest_product_is_odd(l))
+        elif optiune == 3:
+            print(get_longest_concat_digits_asc(l))
+        elif optiune == 4:
+            break
+        else:
+            print("Optiune gresita! Reincercati!")
+
+main()
